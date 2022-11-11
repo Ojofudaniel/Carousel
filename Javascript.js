4b59991efd899fc27bc87a4796fab4ce787f556f
Ojofudaniel <ojofudaniel@gmail.com>
2022-11-11 10:30:15 +0100

    first push

---
+++ Javascript.js
@@ -0,0 +1,24 @@
+var slider_img = document.querySelector('.slider-img');
+
+var images = ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg', 'e.jpg', 'f.jpg' ];
+
+var i = 0;   // current image index
+
+function prev() {
+    if(i <= 0) i = images.length;
+    i--;
+    return setImg();
+
+}
+
+
+function next() {
+    if(i >= images.length-1) i = -1
+    i++;
+    return setImg();
+}
+
+function setImg() {
+    return slider_img.setAttribute('src', 'images/' +images[i]);
+}
+
